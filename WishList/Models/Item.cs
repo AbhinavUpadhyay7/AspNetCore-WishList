58eb19d6bbc31b221c1a4cfc4629540755104b7a
vmadmin <vmadmin@vssql>
2023-01-10 06:18:49 +0000

    Data

--- WishList/Models/Item.cs
+++ WishList/Models/Item.cs
@@ -1,10 +1,14 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace WishList.Models
 {
     public class Item
     {
+
         public int Id { get; set; }
 
         [Required]
