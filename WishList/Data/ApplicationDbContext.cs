8b1880b8b7876e57a88e3ac3e0db0882eee8391f
vmadmin <vmadmin@vssql>
2023-01-10 04:30:28 +0000

    Startup

--- WishList/Data/ApplicationDbContext.cs
+++ WishList/Data/ApplicationDbContext.cs
@@ -1,15 +1,18 @@
 ﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
-using WishList.Data;
 
 namespace WishList.Data
 {
-    public class ApplicationDbContext : DbContext
+    public class ApplicationDbContext:DbContext
     {
-        public ApplicationDbContext(DbContextOptions options) : base(options)
-        {
-        }
-
         public DbSet<Models.Item> Items { get; set; }
+        public ApplicationDbContext(DbContextOptions options):base(options)
+        {
+
+        }
     }
 }
--- WishList/Startup.cs
+++ WishList/Startup.cs
@@ -1,8 +1,16 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using WishList.Data;
+
 namespace WishList
 {
     public class Startup
@@ -12,27 +20,26 @@ namespace WishList
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
-
-        services.AddDbContext<ApplicationDbContext>(options => options.UseInMemoryDatabase("Wishlist"));
+            services.AddDbContext<ApplicationDbContext>(options => options.UseInMemoryDatabase("Wishlist"));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-
             if (env.IsDevelopment())
             {
+
                 app.UseDeveloperExceptionPage();
             }
+
             else
             {
                 app.UseExceptionHandler("/Home/Error");
             }
+
             app.UseRouting();
             app.UseEndpoints(endpoints => { endpoints.MapDefaultControllerRoute(); });
-
-
-
         }
+        
     }
 }
