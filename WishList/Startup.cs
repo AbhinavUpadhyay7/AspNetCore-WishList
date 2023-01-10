d13f3af2b8fb40acb73d9e4dfe265c818ee34250
vmadmin <vmadmin@vssql>
2023-01-10 07:09:13 +0000

    Startup

--- WishList/Startup.cs
+++ WishList/Startup.cs
@@ -25,6 +25,8 @@ namespace WishList
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.Run(async (context) =>
+            
             if (env.IsDevelopment())
             {
 
